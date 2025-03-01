9a0a2bc083468153227d2558982db7331ad22d9b
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2025-03-01 15:37:24 +0400

    added aws-cognito auth

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-
+import AuthProvider from "./AuthProvider";
 import StoreProvider, { useAppSelector } from "./redux";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
@@ -38,9 +38,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-     
+      <AuthProvider>
         <DashboardLayout>{children}</DashboardLayout>
-     
+      </AuthProvider>
     </StoreProvider>
   );
 };
